7eae66d5d04fba5dd9414d3ef8a9005875c087e6
Offirmo <offirmo.net@gmail.com>
2017-02-10 15:02:12 +1300

    +

--- NOTES.md
+++ NOTES.md
@@ -35,3 +35,55 @@ entries:
 -     : parsed config
 -     : parsed content
 -     : password
+
+
+	//const module_name = dep_path.startsWith('@') ? dep_path.split('/').slice(0, 2).join('/') : dep_path.split('/')[0]
+
+
+/*const NEEDED_MODULES = [
+	'@offirmo/rx-auto',
+	'@reactivex/rxjs',
+	'js-yaml',
+	'packery',
+	'require-css',
+	'requirejs',
+	'shave',
+	'tachyons',
+	'tslib',
+	'typescript-string-enums',
+	'when-dom-ready' ]*/
+
+	const NEEDED_FILES_FROM_MODULESx = NEEDED_MODULES.map(mod => require(`${mod}/package.json`).main)
+   console.info(NEEDED_FILES_FROM_MODULESx)
+
+   const NEEDED_FILES_FROM_MODULES = [
+   	// order matters !
+   	'requirejs/require.js',
+   	'require-css/css.js',
+   	'tachyons/css/tachyons.css',
+   	'lodash/lodash.js',
+   	'tslib/tslib.js',
+   	'packery/dist/packery.pkgd.js',
+   	'js-yaml/dist/js-yaml.js',
+   	'typescript-string-enums/dist/index.js',
+   	'@reactivex/rxjs/dist/global/Rx.js',
+   	'@offirmo/rx-auto/dist/es6.umd/index.js',
+   ]
+
+
+
+		/*
+		'@offirmo/rx-auto': 'dist/third-party/offirmo.rx-auto@0',
+		'@reactivex/rxjs' : 'dist/third-party/reactivex.rxjs.rx@5',
+		'js-yaml' : 'dist/third-party/js-yaml@3',
+		'lodash'  : 'dist/third-party/lodash@4',
+		'packery' : 'dist/third-party/packery.pkgd@2',
+		'tachyons': 'dist/src.es7.amd/empty',
+		'tslib'   : 'dist/third-party/tslib@1',
+		'typescript-string-enums': 'dist/third-party/typescript-string-enums@0',
+		*/
+		/*
+		'extended-exceptions'      : 'bower_components/extended-exceptions.js/extended_exceptions',
+		'javascript-state-machine' : 'bower_components/javascript-state-machine/state-machine',
+		'rx'                       : 'bower_components/rxjs/dist/rx.all',
+		*/
--- meta/scripts/copy-third-party.js
+++ meta/scripts/copy-third-party.js
@@ -7,66 +7,86 @@ const path = require('path')
 const semver = require('semver')
 const fs = require('@offirmo/cli-toolbox/fs/extra')
 
-const NEEDED_FILES_FROM_MODULES = [
-	// order matters !
-	'requirejs/require.js',
-	'require-css/css.js',
-	'tachyons/css/tachyons.css',
-	'lodash/lodash.js',
-	'tslib/tslib.js',
-	'packery/dist/packery.pkgd.js',
-	'js-yaml/dist/js-yaml.js',
-	'typescript-string-enums/dist/index.js',
-	'@reactivex/rxjs/dist/global/Rx.js',
-	'@offirmo/rx-auto/dist/es6.umd/index.js',
-]
+function log() {
+	if (false) console.log(...arguments)
+}
+
+const package_json = require('../../package.json')
+
+const NEEDED_MODULES = Object.keys(package_json.dependencies)
+log(NEEDED_MODULES)
 
 const MANAGED_MODULES_DIR = 'node_modules'
 const SOURCE_CONTROLLED_THIRD_PARTY_DIR = 'dist/third-party'
 
 fs.emptyDirSync(SOURCE_CONTROLLED_THIRD_PARTY_DIR)
 
+
 let header_deps = ''
+let requirejs_deps = ''
 
-function log() {
-	if (false) console.log(...arguments)
-}
-
-NEEDED_FILES_FROM_MODULES.forEach(dep_path => {
-	const module_name = dep_path.startsWith('@') ? dep_path.split('/').slice(0, 2).join('/') : dep_path.split('/')[0]
-	log('module name:', module_name)
-
-	const module_version = semver.clean(require(`${module_name}/package.json`).version)
-	log('module version:', module_version)
-
-	const id = (module_name.endsWith('.js') ? module_name.slice(0, -3) : module_name)
+function cleanupModuleNameAndMakeItFilenameFriendly(module_name) {
+	return (module_name.endsWith('.js') ? module_name.slice(0, -3) : module_name)
 		.split('/')
 		.map(s => s.startsWith('@') ? s.slice(1) : s)
 		.join('.')
-	log('(computed) id:', id)
+}
+
+
+NEEDED_MODULES.forEach(module_name => {
+	const package_json = require(`${module_name}/package.json`)
+	log({module_name})
+
+	let dep_path
+	switch(module_name) {
+		case '@reactivex/rxjs': dep_path = `${module_name}/dist/global/Rx.js`; break
+		case 'js-yaml': dep_path = `${module_name}/dist/js-yaml.js`; break
+		case 'packery': dep_path = `${module_name}/dist/packery.pkgd.js`; break
+		case 'require-css': dep_path = `${module_name}/css.js`; break
+		case 'requirejs': dep_path = `${module_name}/require.js`; break
+		default:
+			dep_path = `${module_name}/${package_json.main || 'index.js'}`
+			break
+	}
+	log({dep_path})
+
+	const module_version = semver.clean(package_json.version)
+	log({module_version})
+
+	const ffid = cleanupModuleNameAndMakeItFilenameFriendly(module_name)
+	log({ffid})
 
 	const dep_path_parsed = path.parse(dep_path)
 	log(dep_path_parsed)
 
 	let target_radix = dep_path_parsed.name
-	// cases were information is redondant / superfluous
-	if (target_radix === 'index') target_radix = id // cause adds no info
-	if ((target_radix + dep_path_parsed.ext.slice(1)) === id) target_radix = id // case like requirejs/require.js
+	// cases were information is redundant / superfluous
+	if (target_radix === 'index') target_radix = ffid // cause adds no info
+	if ((target_radix + dep_path_parsed.ext.slice(1)) === ffid) target_radix = ffid // case like requirejs/require.js
 	// cases were information is missing
-	if (! target_radix.includes(id)) target_radix = id + '.' + target_radix
+	if (! target_radix.includes(ffid)) target_radix = ffid + '.' + target_radix
 	// uniformize
-	target_radix = target_radix.slice(target_radix.indexOf(id)).toLocaleLowerCase()
+	target_radix = target_radix.slice(target_radix.indexOf(ffid)).toLocaleLowerCase()
 	log('target_radix:', target_radix)
 
 	let target_filename = target_radix + '@' + module_version + dep_path_parsed.ext
 	let target_filename_major = target_radix + '@' + semver.major(module_version) + dep_path_parsed.ext
-	console.log(module_name, module_version, id, target_radix, semver.major(module_version), target_filename, target_filename_major)
+	console.log({module_name, module_version, dep_path, ffid, target_radix, major: semver.major(module_version), target_filename, target_filename_major})
 
 	fs.copySync(path.join(MANAGED_MODULES_DIR, dep_path), path.join(SOURCE_CONTROLLED_THIRD_PARTY_DIR, target_filename))
 	fs.copySync(path.join(SOURCE_CONTROLLED_THIRD_PARTY_DIR, target_filename), path.join(SOURCE_CONTROLLED_THIRD_PARTY_DIR, target_filename_major))
 
-	if (dep_path_parsed.ext === '.css')
+	if (dep_path_parsed.ext === '.css') {
 		header_deps += `\n<link rel="stylesheet" type="text/css" href="${SOURCE_CONTROLLED_THIRD_PARTY_DIR}/${target_filename_major}" />`
-	else
+		requirejs_deps += `\n'${module_name}': 'dist/src.es7.amd/empty',`
+	}
+	else {
 		header_deps += `\n<script src="${SOURCE_CONTROLLED_THIRD_PARTY_DIR}/${target_filename_major}"></script>`
+		requirejs_deps += `\n'${module_name}': '${SOURCE_CONTROLLED_THIRD_PARTY_DIR}/${target_filename_major.slice(0, -3)}',`
+	}
 })
+
+console.log('\nPut that in your header:')
+console.log(header_deps)
+console.log('\nPut that in your RequireJS config:')
+console.log(requirejs_deps)
